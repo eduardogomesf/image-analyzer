a2618fa9e82405e9d6be8a6583b98fdd4c736638
Eduardo Gomes <edugomesf@icloud.com>
2024-09-08 09:27:38 -0300

    feat: add image analyzer method

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-const message = 'Hello World'
+import { ENVS } from "./config/envs"
+import { genAI } from "./config/gen-ai"
+import { generateImagePromptInput } from "./util"
 
-console.log(message)
+async function run() {
+  const model = genAI.getGenerativeModel({ model: ENVS.google.genAI.model })
+
+  const prompt = "Describe everything you can see in this image"
+
+  const imagePrompt = generateImagePromptInput('./tmp/test.webp', "image/webp")
+
+  const imagePromptList = [imagePrompt]
+
+  const result = await model.generateContent([prompt, ...imagePromptList])
+  const response = result.response.text()
+  console.log(response)
+}
+
+run().catch(console.error)
